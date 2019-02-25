dc8f03d247deb99d555e196f4d3d67907640600c
Vinh Do <vinhdo07@gmail.com>
2019-02-25 18:44:02 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,21 @@
 class Song
   attr_accessor :name, :artist
-
+  @@all = []
   def initialize(name)
      @name = name
+     @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def artist_name
+    if self.artist.class == NilClass
+      nil
+    else
+      self.artist.name
+    end
   end
 
 end
