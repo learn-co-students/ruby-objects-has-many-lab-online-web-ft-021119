772b8350a815681347e068980eb8d0b1c978589b
Jacqi Entzminger <jackie.entz@gmail.com>
2019-02-28 09:27:36 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,14 @@
 class Song 
   attr_accessor :name 
+  @@all = []
   
   def initialize(name)
     @name = name 
+    @@all << self 
   end 
+  
+ # def add_song(song)
+    #@songs << song
+   # song.artist = self 
+ # end 
 end 
